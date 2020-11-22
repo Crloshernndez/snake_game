4710fd5ddd4f84a1d17673b057ec26f7a6d93e95
carlos <hernandez1989carlos@gmail.com>
2020-11-21 20:40:43 -0500

    snake-game version 1.0

---
+++ main.py
@@ -0,0 +1,19 @@
+from game import Game 
+
+def main():
+    game = Game()
+    game.run()
+    quit()
+
+if __name__ == '__main__':
+    main()
+
+
+# pygame.init()
+
+# DISPLAYSURF = pygame.display.set_mode((800, 600))
+# BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+# CLOCK = pygame.time.Clock()
+# pygame.display.set_caption('Wormy')
+
+
